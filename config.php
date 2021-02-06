74274f267350475da6dd04c1559c5487add27aee
Åsmund Stavdahl <asmund.stavdahl@ntnu.no>
2021-02-06 11:02:38 +0100

    remove unused files

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'flow'   => require __DIR__ . '/flow.php',
-    'routes' => require __DIR__ . '/routes.php',
-];
--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '/' => [GeneralController::class, 'home'],
-    '/info' => [GeneralController::class, 'info'],
-    '/square/(?<x>\d+)' => [GeneralController::class, 'square'],
-    '/sentence/(?<items>([^,]+)(,[^,]+)*)' => [GeneralController::class, 'sentence'],
-];
